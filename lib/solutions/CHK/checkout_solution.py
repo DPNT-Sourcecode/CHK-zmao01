dfdb7dd43db185a0b9b9484cbaff39b54a28aa50
sbx_user1051 <sbx_user1051@169.254.14.37>
2022-05-26 17:53:41 +0000

    Thu May 26 17:53:41 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,15 @@
 import re
 
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+
 
 # noinspection PyUnusedLocal
 # skus = unicode string
@@ -11,28 +21,72 @@ def checkout(skus: str) -> int:
 
     # this would usually come from a db or similar
     prices = {
-        "A": {"price": 50, "offer": (3, 130)},
-        "B": {"price": 30, "offer": (2, 45)},
-        "C": {"price": 20, "offer": None},
-        "D": {"price": 15, "offer": None},
+        "A": {"price": 50, "offers": [(5, 200, "discount"), (3, 130, "discount")]},
+        "E": {"price": 40, "offers": [(2, (1, "B"), "free_item")]},
+        "B": {"price": 30, "offers": [(2, 45, "discount")]},
+        "C": {"price": 20, "offers": None},
+        "D": {"price": 15, "offers": None},
     }
 
     valid_skus = ["A", "B", "C", "D"]
     if not re.match(r"^[A,B,C,D]*$", skus):
         return -1
     total_cost = 0
+    free_items = {}
     for sku in valid_skus:
         quantity = skus.count(sku)
         if quantity > 0:
-            if prices[sku]["offer"]:
-                offer_quantity, offer_price = prices[sku]["offer"]
-                if quantity >= offer_quantity:
-                    match_offer_quantity = quantity // offer_quantity
-                    reminder = quantity - (match_offer_quantity * offer_quantity)
-                    total_cost += (reminder * prices[sku]["price"]) + (match_offer_quantity * offer_price)
+            if sku in free_items:
+                quantity -= free_items[sku]
+            for offer in prices[sku]["offers"]:
+                offer_quantity, offer_price, offer_type = offer
 
-                    continue
+                if quantity >= offer_quantity:
+                    if offer_type == "discount":
+                        match_offer_quantity = quantity // offer_quantity
+                        reminder = quantity - (match_offer_quantity * offer_quantity)
+                        total_cost += (reminder * prices[sku]["price"]) + (match_offer_quantity * offer_price)
+                        continue
+                    elif offer_type == "free_item":
+                        free_item, free_item_quantity = offer_quantity
+                        free_items[free_item] = free_item_quantity
 
             total_cost += quantity * prices[sku]["price"]
 
     return total_cost
+
+
+# def checkout_1(skus: str) -> int:
+#     """
+#     sku: str, list of sku, count is given by amount of letters
+#
+#     """
+#
+#     # this would usually come from a db or similar
+#     prices = {
+#         "A": {"price": 50, "offer": (3, 130)},
+#         "B": {"price": 30, "offer": (2, 45)},
+#         "C": {"price": 20, "offer": None},
+#         "D": {"price": 15, "offer": None},
+#     }
+#
+#     valid_skus = ["A", "B", "C", "D"]
+#     if not re.match(r"^[A,B,C,D]*$", skus):
+#         return -1
+#     total_cost = 0
+#     for sku in valid_skus:
+#         quantity = skus.count(sku)
+#         if quantity > 0:
+#             if prices[sku]["offer"]:
+#                 offer_quantity, offer_price = prices[sku]["offer"]
+#                 if quantity >= offer_quantity:
+#                     match_offer_quantity = quantity // offer_quantity
+#                     reminder = quantity - (match_offer_quantity * offer_quantity)
+#                     total_cost += (reminder * prices[sku]["price"]) + (match_offer_quantity * offer_price)
+#
+#                     continue
+#
+#             total_cost += quantity * prices[sku]["price"]
+#
+#     return total_cost
+
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,56 @@
+import pytest
+from solutions.CHK import checkout_solution
+
+# +------+-------+------------------------+
+# | Item | Price | Special offers         |
+# +------+-------+------------------------+
+# | A    | 50    | 3A for 130, 5A for 200 |
+# | B    | 30    | 2B for 45              |
+# | C    | 20    |                        |
+# | D    | 15    |                        |
+# | E    | 40    | 2E get one B free      |
+# +------+-------+------------------------+
+
+
+class TestCheckout():
+    def test_checkout_invalid(self):
+        assert checkout_solution.checkout("3qwgyh5") == -1
+
+    @pytest.mark.parametrize(
+        "skus,expected", [
+            ("AAAAAAAAA", 380),
+            ("CCCCD", 95),
+            ("A", 50),
+            ("AAA", 130),
+            ("AAAA", 180),
+            ("AAAAB", 210),
+            ("BBA", 95),
+            ("BC", 50),
+            ("AAB", 130),
+            ("AAAB", 160),
+        ]
+    )
+    def test_checkout_happy(self, skus: str, expected: int):
+        assert checkout_solution.checkout(skus) == expected
+
+
+# class TestCheckout1():
+#     def test_checkout_invalid(self):
+#         assert checkout_solution.checkout("3qwgyh5") == -1
+#
+#     @pytest.mark.parametrize(
+#         "skus,expected", [
+#             ("CCCCC", 100),
+#             ("CCCCD", 95),
+#             ("A", 50),
+#             ("AAA", 130),
+#             ("AAAA", 180),
+#             ("AAAAB", 210),
+#             ("BBA", 95),
+#             ("BC", 50),
+#             ("AAB", 130),
+#             ("AAAB", 160),
+#         ]
+#     )
+#     def test_checkout_happy(self, skus: str, expected: int):
+#         assert checkout_solution.checkout(skus) == expected
