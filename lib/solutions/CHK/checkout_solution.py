cf3dbe141a84388e9efaf4e252c80a83e4f740c6
sbx_user1051 <sbx_user1051@169.254.119.245>
2022-05-28 18:01:26 +0000

    Sat May 28 18:01:26 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -35,6 +35,7 @@ import re
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
 def checkout(skus: str) -> int:
     """
     sku: str, list of sku, count is given by amount of letters
@@ -53,7 +54,7 @@ def checkout(skus: str) -> int:
         "H": {"price": 10, "offers": [(10, 80, "discount"), (5, 45, "discount")]},
         "I": {"price": 35, "offers": []},
         "J": {"price": 60, "offers": []},
-        "K": {"price": 80, "offers": [(2, 150, "discount")]},
+        "K": {"price": 70, "offers": [(2, 150, "discount")]},
         "L": {"price": 90, "offers": []},
         "M": {"price": 15, "offers": []},
         "N": {"price": 40, "offers": [(3, ("M", 1), "free_item")]},
@@ -61,14 +62,20 @@ def checkout(skus: str) -> int:
         "P": {"price": 50, "offers": [(5, 200, "discount")]},
         "Q": {"price": 30, "offers": [(3, 80, "discount")]},
         "R": {"price": 50, "offers": [(3, ("Q", 1), "free_item")]},
-        "S": {"price": 30, "offers": []},
+        "S": {"price": 20, "offers": []},
         "T": {"price": 20, "offers": []},
         "U": {"price": 40, "offers": [(4, 120, "discount")]},
         "V": {"price": 50, "offers": [(3, 130, "discount"), (2, 90, "discount")]},
         "W": {"price": 20, "offers": []},
-        "X": {"price": 90, "offers": []},
-        "Y": {"price": 10, "offers": []},
-        "Z": {"price": 50, "offers": []},
+        "X": {"price": 17, "offers": []},
+        "Y": {"price": 20, "offers": []},
+        "Z": {"price": 21, "offers": []},
+    }
+
+    group_offer = {
+        "skus": ["Z", "T", "S", "Y", "X"],
+        "quantity": 3,
+        "price": 45
     }
 
     valid_skus = ["A", "E", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "N", "M", "O", "P", "R", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -76,30 +83,127 @@ def checkout(skus: str) -> int:
         return -1
     total_cost = 0
     free_items = {}
+
+    group_offers = {
+        "total_quantity": 0,
+        "skus_quantity": {}
+    }
+
+    # applying group offer before processing cart
+    for sku in group_offer['skus']:
+        sku_quantity = skus.count(sku)
+        group_offers["total_quantity"] += sku_quantity
+        group_offers["skus_quantity"][sku] = sku_quantity
+
+    match_group_offer_quantity = group_offers["total_quantity"] // group_offer["quantity"]
+    to_process = group_offers["total_quantity"] - (match_group_offer_quantity * group_offer["quantity"])
+    total_cost += (match_group_offer_quantity * group_offer["quantity"])
+
+    for sku, quantity in group_offers["skus_quantity"].items()
+
+
+
     for sku in valid_skus:
+        # count amount for each sku
         quantity = skus.count(sku)
         if quantity > 0:
+            # only process skus that are in the cart
             if sku in free_items:
+                # remove any free items from previous offers
                 quantity -= free_items[sku]
             to_process = quantity
+
             for offer in prices[sku]["offers"]:
+                # if the sku has an offer, process it
                 offer_quantity, offer_price, offer_type = offer
 
                 if to_process >= offer_quantity:
+                    # only process if the sku matches the required quantity to trigger the offer
+                    # (this relies on offers being in order)
                     if offer_type == "discount":
+                        # if the order is a simple discount, apply it, treat sku that don't trigger offer normally
                         match_offer_quantity = to_process // offer_quantity
                         to_process = to_process - (match_offer_quantity * offer_quantity)
                         total_cost += (match_offer_quantity * offer_price)
                         continue
                     elif offer_type == "free_item":
+                        # if the offer give a free item, add the free item to the list of free skus.
+                        # this too relies on the order in which skus are processed
                         match_offer_quantity = to_process // offer_quantity
                         free_item, free_item_quantity = offer_price
                         free_items[free_item] = free_item_quantity*match_offer_quantity
 
             if to_process > 0:
+                # this code deal with sku with no offers (and leftover skus from items that had offers)
                 total_cost += (to_process * prices[sku]["price"])
 
     return total_cost
+
+# def checkout_4(skus: str) -> int:
+#     """
+#     sku: str, list of sku, count is given by amount of letters
+#
+#     """
+#
+#     # this would usually come from a db or similar
+#     prices = {
+#         "A": {"price": 50, "offers": [(5, 200, "discount"), (3, 130, "discount")]},
+#         "B": {"price": 30, "offers": [(2, 45, "discount")]},
+#         "C": {"price": 20, "offers": []},
+#         "D": {"price": 15, "offers": []},
+#         "E": {"price": 40, "offers": [(2, ("B", 1), "free_item")]},
+#         "F": {"price": 10, "offers": [(3, 20, "discount")]},
+#         "G": {"price": 20, "offers": []},
+#         "H": {"price": 10, "offers": [(10, 80, "discount"), (5, 45, "discount")]},
+#         "I": {"price": 35, "offers": []},
+#         "J": {"price": 60, "offers": []},
+#         "K": {"price": 80, "offers": [(2, 150, "discount")]},
+#         "L": {"price": 90, "offers": []},
+#         "M": {"price": 15, "offers": []},
+#         "N": {"price": 40, "offers": [(3, ("M", 1), "free_item")]},
+#         "O": {"price": 10, "offers": []},
+#         "P": {"price": 50, "offers": [(5, 200, "discount")]},
+#         "Q": {"price": 30, "offers": [(3, 80, "discount")]},
+#         "R": {"price": 50, "offers": [(3, ("Q", 1), "free_item")]},
+#         "S": {"price": 30, "offers": []},
+#         "T": {"price": 20, "offers": []},
+#         "U": {"price": 40, "offers": [(4, 120, "discount")]},
+#         "V": {"price": 50, "offers": [(3, 130, "discount"), (2, 90, "discount")]},
+#         "W": {"price": 20, "offers": []},
+#         "X": {"price": 90, "offers": []},
+#         "Y": {"price": 10, "offers": []},
+#         "Z": {"price": 50, "offers": []},
+#     }
+#
+#     valid_skus = ["A", "E", "B", "C", "D", "F", "G", "H", "I", "J", "K", "L", "N", "M", "O", "P", "R", "Q", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+#     if not re.match(r"^[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]*$", skus):
+#         return -1
+#     total_cost = 0
+#     free_items = {}
+#     for sku in valid_skus:
+#         quantity = skus.count(sku)
+#         if quantity > 0:
+#             if sku in free_items:
+#                 quantity -= free_items[sku]
+#             to_process = quantity
+#             for offer in prices[sku]["offers"]:
+#                 offer_quantity, offer_price, offer_type = offer
+#
+#                 if to_process >= offer_quantity:
+#                     if offer_type == "discount":
+#                         match_offer_quantity = to_process // offer_quantity
+#                         to_process = to_process - (match_offer_quantity * offer_quantity)
+#                         total_cost += (match_offer_quantity * offer_price)
+#                         continue
+#                     elif offer_type == "free_item":
+#                         match_offer_quantity = to_process // offer_quantity
+#                         free_item, free_item_quantity = offer_price
+#                         free_items[free_item] = free_item_quantity*match_offer_quantity
+#
+#             if to_process > 0:
+#                 total_cost += (to_process * prices[sku]["price"])
+#
+#     return total_cost
 
 
 
@@ -229,3 +333,4 @@ def checkout(skus: str) -> int:
 #             total_cost += quantity * prices[sku]["price"]
 #
 #     return total_cost
+
